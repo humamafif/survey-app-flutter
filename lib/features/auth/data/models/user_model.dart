c5028d66d59042265ce623d24b3dd99ec9071979
Humam Afif Al-Mahbubi <120432634+humamafif@users.noreply.github.com>
2025-03-18 23:41:11 +0700

    Revert "implement firebase"

--- lib/features/auth/data/models/user_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:survey_app/features/auth/domain/entities/user_entity.dart';
-
-class UserModel extends UserEntity {
-  const UserModel({required super.uid, super.email});
-
-  factory UserModel.fromFirebase(dynamic user) {
-    return UserModel(uid: user.uid, email: user.email);
-  }
-}
--- lib/features/auth/domain/repositories/auth_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:survey_app/core/error/failure.dart';
-import 'package:survey_app/features/auth/domain/entities/user_entity.dart';
-
-abstract class AuthRepository {
-  Future<Either<Failure, UserEntity>> signIn(String email, String password);
-  Future<Either<Failure, UserEntity>> signUp(String email, String password);
-  Future<Either<Failure, void>> signOut();
-}
